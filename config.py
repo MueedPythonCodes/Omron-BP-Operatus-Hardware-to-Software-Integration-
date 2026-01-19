08eb4cbc01673ba9a7edb3b27ba43c462a0c64b0
Python Coder <151621155+MueedPythonCodes@users.noreply.github.com>
2026-01-19 08:32:28 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,17 @@ import os
 
 class Config:
     # 1. New Sheet ID (Aapke browser URL se li gayi hai)
-    GOOGLE_SHEET_ID = '1gyKVcTcjLxF8DQqTT6u34f66OsxeTzuEr1K5eAKOG80'
+    GOOGLE_SHEET_ID = 'Put your Google Sheet ID here'
     
     # 2. Exact Tab Name (Jo niche neelay rang mein hai)
-    WORKSHEET_NAME = 'BP_Readings'
+    WORKSHEET_NAME = 'Put your worksheet name here'
     
     # Baaki settings wahi rahengi
     GOOGLE_CREDENTIALS_FILE = 'service_account.json'
     DATA_FILE = 'ubpm.json'
     DEVICE_NAME = 'hem-7361t'
-    MAC_ADDRESS = '00:5F:BF:2B:63:A8'
+    MAC_ADDRESS = 'your device mac address'
+
 
 
 
